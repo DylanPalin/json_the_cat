d171d361451d5e23606331fcea41d70ed1d40177
Dylan Palin <dylanpalin@gmail.com>
2023-11-24 10:15:25 -0700

    completed refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
 
-let breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.error('Request failed:', error);
-  }
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(`Request failed:, ${response.statusCode}`, null);
+      return error;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`No breed found for: ${breedName}`, null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
 
+module.exports = { fetchBreedDescription };
